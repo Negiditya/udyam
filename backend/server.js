7110a1ab84e14229371b0450fa4dbe31d13fc92a
Negiditya <anegi1509@gmai.com>
2025-08-14 23:58:43 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const formRoutes = require('./routes/formRoutes');
-
-require('dotenv').config();
+const express = require('express')
+const cors = require("cors");
+const formRoutes = require('./routes/formRoutes')
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://udyam-opal.vercel.app'],
-  credentials: true
+    origin: ['http://localhost:5173', 'https://udyam-opal.vercel.app'],
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 app.use(express.json());
-app.use('/api', formRoutes);
+
+app.get('/', (req, res) => {
+    res.send("server is running")
+})
+
+app.use('/api', formRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
